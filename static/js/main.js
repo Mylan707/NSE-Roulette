1f9291cf3dfbed07093af3f43b60816489cdaeda
Mylan707 <mylaneling6@gmail.com>
2026-02-20 11:05:51 +0100

    Fix: use correct European roulette wheel order (standard EU bets)

--- static/js/main.js
+++ static/js/main.js
@@ -10,8 +10,9 @@ let gameState = {
 const RED_NUMBERS = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36];
 
 // Real European roulette wheel number order (clockwise), starting with 0
+// This is the standard European roulette numbering (not American)
 const WHEEL_ORDER = [
-    0,32,15,19,4,21,2,25,17,34,6,27,13,36,11,30,8,23,10,5,24,16,33,1,20,14,31,9,22,18,29,7,28,12,35,3,26
+    0, 26, 3, 35, 12, 28, 7, 29, 18, 22, 9, 31, 14, 20, 1, 33, 16, 24, 5, 10, 23, 8, 30, 11, 36, 13, 27, 6, 34, 17, 25, 2, 21, 4, 19, 15, 32
 ];
 // Chip colors based on bet amount
 const CHIP_COLORS = {
